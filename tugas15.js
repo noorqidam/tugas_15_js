009f0a949f759fbe109e277f65b2b7668c4069cc
noorqidam <noorqidam@gmail.com>
2019-09-03 15:03:34 +0700

    Tugas 15 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+function tugas15(){
+  console.log("Nilai AB : ",8)
+  console.log("Nilai AC : ",6)
+
+  var c = Math.sqrt(Math.pow(8,2)+(6,2));
+  console.log("Panjang sisi AC adalah : ",c," cm");
+}
+tugas15()
